00f744e89e27a8842e7bf22181eaafe0ea22a275
Zach Feldmann <snipedhype.ai@gmail.com>
2024-09-15 20:26:09 -0500

    Script to merge PDF's

---
+++ script.js
@@ -0,0 +1,17 @@
+const { PDFDocument } = require('pdf-lib');
+const fs = require('fs');
+
+async function mergePDFs(pdfPaths, outputPath) {
+  const mergedPDF = await PDFDocument.create();
+
+  for (let pdfPath of pdfPaths) {
+    const pdfData = fs.readFileSync(pdfPath);
+    const pdfToMerge = await PDFDocument.load(pdfData);
+    const copiedPages = await mergedPDF.copyPages(pdfToMerge, pdfToMerge.getPageIndices());
+    copiedPages.forEach((page) => mergedPDF.addPage(page));
+  }
+  const finalPDFData = await mergedPDF.save();
+  fs.writeFileSync(outputPath, finalPDFData);
+
+  console.log(`check output file for merged pdf: ${outputPath}`);
+}
